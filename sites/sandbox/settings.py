8e107b7913202e6f7de2f4bf79673f6a5c685d92
ajit <ajitpatil1202@gmail.com>
2014-07-11 20:48:47 +0530

    ajit1

--- sites/sandbox/settings.py
+++ sites/sandbox/settings.py
@@ -39,14 +39,15 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'virt7',
-        'USER': 'pavan',
-        'PASSWORD': 'vivenkyan',
+        'NAME': 'HOHDB1',
+        'USER': 'postgres',
+        'PASSWORD': 'ajit',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         #'ATOMIC_REQUESTS': True
     }
 }
+
 
 CACHES = {
     'default': {
