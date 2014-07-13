9f160f30f0675ee21469e151f1b3263e686d4adf
vivenkyan <vivenkyan@gmail.com>
2014-07-13 16:49:10 +0530

    removing m2m inline error

--- sites/sandbox/settings.py
+++ sites/sandbox/settings.py
@@ -39,15 +39,15 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-<<<<<<< HEAD
+# <<<<<<< HEAD
         'NAME': 'workoscar',
         'USER': 'pavan',
         'PASSWORD': 'vivenkyan',
-=======
-        'NAME': 'HOHDB1',
-        'USER': 'postgres',
-        'PASSWORD': 'ajit',
->>>>>>> ae0af626c8fd70a1c7b9831db888c5253f72cb9b
+# =======
+#         'NAME': 'HOHDB1',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ajit',
+# >>>>>>> ae0af626c8fd70a1c7b9831db888c5253f72cb9b
         'HOST': '127.0.0.1',
         'PORT': '5432',
         #'ATOMIC_REQUESTS': True
